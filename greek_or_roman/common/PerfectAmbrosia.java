90c7969e24db29f626f4acb636fa9d84207d71ab
lionbryce <lionbryce101@gmail.com>
2013-04-05 12:21:12 -0400

    has the chance of posion I, but will give you Regeneration II

--- greek_or_roman/common/PerfectAmbrosia.java
+++ greek_or_roman/common/PerfectAmbrosia.java
@@ -2,6 +2,7 @@ package greek_or_roman.common;
 
 import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.item.ItemFood;
+import net.minecraft.potion.Potion;
 
 public class PerfectAmbrosia extends ItemFood
 {
@@ -9,6 +10,9 @@ public class PerfectAmbrosia extends ItemFood
 	public PerfectAmbrosia(int par1, int par2, float par3, boolean par4)
 	{
 		super(par1, par2, par3, par4);
+        this.setAlwaysEdible();
+        this.setPotionEffect(Potion.regeneration.id, 600, 3, 1.0f);
+        this.setPotionEffect(Potion.poison.id, 600, 2, 0.1f);
 	}
 	
     @Override
