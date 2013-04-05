d311bcfee661682f823a8112e616a6b38c5d3c65
lionbryce <lionbryce101@gmail.com>
2013-04-05 12:20:24 -0400

    automattically gives you poison I, but can give you regeneration I, regeneration II, and it can give you poison II

--- greek_or_roman/common/Ambrosia.java
+++ greek_or_roman/common/Ambrosia.java
@@ -2,6 +2,7 @@ package greek_or_roman.common;
 
 import net.minecraft.client.renderer.texture.IconRegister;
 import net.minecraft.item.ItemFood;
+import net.minecraft.potion.Potion;
 
 public class Ambrosia extends ItemFood
 {
@@ -9,6 +10,10 @@ public class Ambrosia extends ItemFood
 	public Ambrosia(int par1, int par2, float par3, boolean par4)
 	{
 		super(par1, par2, par3, par4);
+        this.setPotionEffect(Potion.regeneration.id, 120, 2, 0.50f);
+        this.setPotionEffect(Potion.poison.id, 120, 1, 1.0f);
+        this.setPotionEffect(Potion.regeneration.id, 120, 1, 1.0f);
+        this.setPotionEffect(Potion.poison.id, 120, 2, 0.25f);
 	}
 	
     @Override
